41a43976a9133e219ad70c143828ceb648d2247f
R-Magnotti <richard.magnotti@gmail.com>
2021-02-14 19:33:43 -0500

    Added nav bar and visualization space

--- pages/chatbot.js
+++ pages/chatbot.js
@@ -1,10 +1,11 @@
 import ChatBot from "react-simple-chatbot"
 import { ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader} from 'react-pro-sidebar';
 import 'react-pro-sidebar/dist/css/styles.css';
-import Head from "next/head"
-import AgentAnswer from "./AgentAnswer"
+import Head from "next/head";
+import AgentAnswer from "./AgentAnswer";
 import { BsBook, BsQuestionCircle, BsGearWideConnected } from "react-icons/bs";
-
+import { Navbar, NavItem, NavDropdown, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
 
 var latest_answer_options;
 
@@ -264,45 +265,69 @@ export default function ChatBotPage() {
     ];
 
     return (
-        <div class="container">
-	        <ProSidebar collapsed="collapsed" style={{height: "100vh", display: "inline-block"}}>
-	      	<SidebarHeader>
-		        <div	
-		          style={{
-		            padding: '24px',
-		            textTransform: 'uppercase',
-		            fontWeight: 'bold',
-		            fontSize: 14,
-		            letterSpacing: '1px',
-		            overflow: 'hidden',
-		            textOverflow: 'ellipsis',
-		            whiteSpace: 'nowrap',
-		          }}>	
-		        <BsGearWideConnected iconShape="cicrle" />
-	        	</div>
-      		</SidebarHeader>
-			  <Menu>
-			    <MenuItem class="story" iconShape="cicrle" icon={<BsBook />}></MenuItem>
-			    <MenuItem class="story" iconShape="cicrle" icon={<BsQuestionCircle />}></MenuItem>
-			  </Menu>
-			</ProSidebar>
+        <div class="container" style={{width: "100%", margin: "0", padding: "0"}}>
+		<Navbar bg="light" style={{width: "100%", "white-space": "no-wrap" }}expand="lg">
+		  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+		  <Navbar.Collapse id="basic-navbar-nav">
+		    <Nav className="mr-auto">
+		      <Nav.Link href="#home">Home</Nav.Link>
+		      <Nav.Link href="#link">Link</Nav.Link>
+		      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+		        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+		        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+		        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+		        <NavDropdown.Divider />
+		        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+		      </NavDropdown>
+		    </Nav>
+		    <Form inline>
+		      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+		      <Button variant="outline-success">Search</Button>
+		    </Form>
+		  </Navbar.Collapse>
+		</Navbar>
 
-        	<div class="chatbot" style={{float: "right", width: "95%", margin: "5px", height: "100vh", display: "inline-block"}}>
-	            <Head>
-	                <title>Co-Reasoning ChatBot</title>
-	                <link rel="icon" href="/favicon.ico" />
-	            </Head>
-	            <main>
-	                <ChatBot
-	                    headerTitle="Collaboration with AI"
-	                    // speechSynthesis={{ enable: true, lang: 'en' }}
-	                    steps={chat_steps}
-	                    width="auto"
-	                    height="75vh"
-	                    fontFamily="Helvetica"
-	                />
-	            </main>
-	        </div>
+			<div style={{display: "flex", "justify-content": "space-between"}}>
+		        <ProSidebar collapsed="collapsed" style={{height: "100vh", display: "inline-block", float: "left"}}>
+		      	<SidebarHeader>
+			        <div	
+			          style={{
+			            padding: '24px',
+			            textTransform: 'uppercase',
+			            fontWeight: 'bold',
+			            fontSize: 14,
+			            letterSpacing: '1px',
+			            overflow: 'hidden',
+			            textOverflow: 'ellipsis',
+			            whiteSpace: 'nowrap',
+			          }}>	
+			        <BsGearWideConnected iconShape="cicrle" />
+		        	</div>
+	      		</SidebarHeader>
+				  <Menu>
+				    <MenuItem class="story" iconShape="cicrle" icon={<BsBook />}></MenuItem>
+				    <MenuItem class="story" iconShape="cicrle" icon={<BsQuestionCircle />}></MenuItem>
+				  </Menu>
+				</ProSidebar>
+
+	        	<div class="chatbot" style={{height: "100vh", display: "block", margin: "0 auto", width: "70%"}}>
+		            <Head>
+		                <title>Co-Reasoning ChatBot</title>
+		                <link rel="icon" href="/favicon.ico" />
+		            </Head>
+		            <main>
+		                <ChatBot
+		                    headerTitle="Collaboration with AI"
+		                    // speechSynthesis={{ enable: true, lang: 'en' }}
+		                    steps={chat_steps}
+		                    fontFamily="Helvetica"
+		                    width="100%"
+		                    height="100vh"
+		                />
+		            </main>
+		        </div>
+		        <div class="visualization" style={{float: "right", "border-style": "solid", width: "30%"}}>Visual</div>
+		    </div>
         </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 export default function Home() {
   return (
