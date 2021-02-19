a9d7cfb6d7d4e7b7b1fbc5bbfd266f57928f4d22
R-Magnotti <richard.magnotti@gmail.com>
2021-02-19 01:07:56 -0500

    reorganized page. Added repeated dialogue branch for reasoning step triplet acceptance. Reasoning component does not update.

--- pages/chatbot.js
+++ pages/chatbot.js
@@ -17,7 +17,7 @@ var reasoning = [
 				{type: "edge", load: "motivates"},
 				{type: "node", load: "Gina pretends to be grateful"}
 				];
-var iteration = 0;				
+global.step = 0;		
 
 export function prepareOptionList(answers){
     var options = []
@@ -27,139 +27,193 @@ export function prepareOptionList(answers){
     latest_answer_options = options;
 }
 
-export default function ChatBotPage() {
-	const Button = props => (
-		<>
-			<h1>{reasoning[iteration].load}</h1>
-	      <button onClick={() => props.triggerNextStep({trigger: 'cont-human-need'})}>
-	        This makes sense. 
-	      </button>
-	      <button onClick={() => props.triggerNextStep({trigger: 'welcome-msg2'})}>
-	        No, this is wrong.
-	      </button>
-	    </>
+function stepper(){
+	global.step += 2;
+};
+function stepChecker(){
+	if (step > reasoning.length-2){	//meaning there is less than 1 whole step left
+	}
+};
+
+const ButtonHumanNeed = props => (
+	<>
+		<h1>{reasoning[step].load}</h1>
+      <button onClick={() => {props.triggerNextStep({trigger: 'last-of-human'});}}>
+        This makes sense. 
+      </button>
+      <button onClick={() => {props.triggerNextStep({trigger: 'welcome-msg2'});}}>
+        No, this is wrong.
+      </button>
+    </>
 );
 
+const Logic = props => (
+	<div style={{height: "12vh", display: "block", width: "100%", position: "relative", "text-align": "center"}}>
+		<p>{reasoning[global.step].load}</p>
+		<p>{reasoning[global.step + 1].load}</p>
+		<p>{reasoning[global.step + 2].load}</p>
+	</div>
+);
+
+const ButtonReasoningLogic = props => (
+	<>
+      <button onClick={() => {props.triggerNextStep({trigger: 'accept-logic'}); stepper();}}>
+        This makes sense. 
+      </button>
+      <button onClick={() => {props.triggerNextStep({trigger: 'cont-human-need'});}}>
+        No, this is wrong.
+      </button>
+    </>
+);
+
+export default function ChatBotPage() {
     const chat_steps = [
-    	/*tutorial / intro***************************************************/
-    	{
-            id: "tut-intro",
-            message: "Hello, I am an AI chatbot named 'Zheng-bot 3000'. I will begin by teaching you some of the basics.",
-            trigger: "intro-pane"
-        },
-    	{
-            id: "intro-pane",
-            component: (
-                <div>
-                    <div>
-                    	In the visualization window, blocks of this color {" "}
-                        <img src="/node_display.png" /><br/>
-                    	represent statements or claims.<br/><br/>
-                    	For example:<br/>
-                    	<img src="/example_statement.png"/>
-                    </div>
-                </div>
-            ),
-            asMessage: true,
-            trigger: "intro-pane-2"
-        },
-        {
-            id: "intro-pane-2",
-            component: (
-                <div>
-                    <div>
-                    	Additionally, blocks of this color {" "}
-                        <img src="/edge_display.png" />{" "} <br/>
-                    	represent relationships between claims<br/><br/>
-                    	For example:<br/>
-                    	<img src="/example_edge.png"/>
-                	</div>
-                </div>
-            ),
-            asMessage: true,
-            trigger: "intro-pane-3",
-        },
-        {
-            id: "intro-pane-3",
-            component: (
-                <div>
-                    <div>
-                    	Where the overall structure of the logic, connecting two claims would look like:<br/>
-                    	<img src="/overall_logic.png"/>
-                	</div>
-                </div>
-            ),
-            asMessage: true,
-            trigger: "intro-tool-msg",
-        },
-        {
-            id: "intro-tool-msg",
-            message: "You can also access task-related information via the toolbar.",
-            trigger: "intstruction-confirm"
-        },
-      	{
-		    id: 'intstruction-confirm',
-		    options: 
-		    [
-		      { value: 1, label: "Proceed", trigger: 'welcome-msg' },
-		    ],
-	    },
-		/****************************************************/
+  //   	/****************************************************/
+  //   	/*tutorial / intro*/
+  //   	/****************************************************/
+  //   	{
+  //           id: "tut-intro",
+  //           message: "Hello, I am an AI chatbot named 'Zheng-bot 3000'. I will begin by teaching you some of the basics.",
+  //           trigger: "intro-pane"
+  //       },
+  //   	{
+  //           id: "intro-pane",
+  //           component: (
+  //               <div>
+  //                   <div>
+  //                   	In the visualization window, blocks of this color {" "}
+  //                       <img src="/node_display.png" /><br/>
+  //                   	represent statements or claims.<br/><br/>
+  //                   	For example:<br/>
+  //                   	<img src="/example_statement.png"/>
+  //                   </div>
+  //               </div>
+  //           ),
+  //           asMessage: true,
+  //           trigger: "intro-pane-2"
+  //       },
+  //       {
+  //           id: "intro-pane-2",
+  //           component: (
+  //               <div>
+  //                   <div>
+  //                   	Additionally, blocks of this color {" "}
+  //                       <img src="/edge_display.png" />{" "} <br/>
+  //                   	represent relationships between claims<br/><br/>
+  //                   	For example:<br/>
+  //                   	<img src="/example_edge.png"/>
+  //               	</div>
+  //               </div>
+  //           ),
+  //           asMessage: true,
+  //           trigger: "intro-pane-3",
+  //       },
+  //       {
+  //           id: "intro-pane-3",
+  //           component: (
+  //               <div>
+  //                   <div>
+  //                   	Where the overall structure of the logic, connecting two claims would look like:<br/>
+  //                   	<img src="/overall_logic.png"/>
+  //               	</div>
+  //               </div>
+  //           ),
+  //           asMessage: true,
+  //           trigger: "intro-tool-msg",
+  //       },
+  //       {
+  //           id: "intro-tool-msg",
+  //           message: "You can also access task-related information via the toolbar.",
+  //           trigger: "intstruction-confirm"
+  //       },
+  //     	{
+		//     id: 'intstruction-confirm',
+		//     options: 
+		//     [
+		//       { value: 1, label: "Proceed", trigger: 'welcome-msg' },
+		//     ],
+	 //    },
+		// /****************************************************/
 
 
-        {
-            id: "welcome-msg",
-            message: "Let's get started analyzing the story. First, your opinion on the human need selection?",
-            trigger: "welcome"
-        },
-        {
-            id: "welcome",
-            component: <Button />
-        },
+  //       {
+  //           id: "welcome-msg",
+  //           message: "Let's get started analyzing the story. First, your opinion on the human need selection?",
+  //           trigger: "welcome"
+  //       },
+  //       {
+  //           id: "welcome",
+  //           component: <ButtonHumanNeed />
+  //       },
 
-        /*path if user does not accept human need***************************************************/
-        {
-            id: "welcome-msg2",
-            message: "I'd like your help to understand what you find wrong about my reasoning process. Please first select a human need that you find appropriate.",
-            trigger: "proceed-human-need"
-        },
-		{
-			id: "proceed-human-need",
-            options: [
-                { value: 'Food', label: 'Food', trigger: "confirm-new-human-need" },
-                { value: 'Social contact', label: 'Social contact', trigger: "confirm-new-human-need" },
-                { value: 'Health', label: 'Health', trigger: "confirm-new-human-need" },
-                { value: 'Savings', label: 'Savings', trigger: "confirm-new-human-need" },
-                { value: 'Approval', label: 'Approval', trigger: "confirm-new-human-need" },
-                { value: 'Order', label: 'Order', trigger: "confirm-new-human-need" },
-                { value: 'Safety', label: 'Safety', trigger: "confirm-new-human-need" },
-                { value: 'Romance', label: 'Romance', trigger: "confirm-new-human-need" },
-                { value: 'Belonging', label: 'Belonging', trigger: "confirm-new-human-need" },
-                { value: 'Power', label: 'Power', trigger: "confirm-new-human-need" },
-                { value: 'Safety', label: 'Safety', trigger: "confirm-new-human-need" },
-                { value: 'Family', label: 'Family', trigger: "confirm-new-human-need" },
-                { value: 'Competition', label: 'Competition', trigger: "confirm-new-human-need" },
-                { value: 'Honor', label: 'Honor', trigger: "confirm-new-human-need" },
-                { value: 'Status', label: 'Status', trigger: "confirm-new-human-need" },
-                { value: 'Curiosity', label: 'Curiosity', trigger: "confirm-new-human-need" },
-                { value: 'Serenity', label: 'Serenity', trigger: "confirm-new-human-need" },
-                { value: 'Idealism', label: 'Idealism', trigger: "confirm-new-human-need" },
-                { value: 'Independence', label: 'Independence', trigger: "confirm-new-human-need" },
-            ]
-		},
-		/****************************************************/
+  //       /****************************************************/
+  //       /*path for human need selection / acceptance*/
+  //       /****************************************************/
+  //       {
+  //           id: "welcome-msg2",
+  //           message: "I'd like your help to understand what you find wrong about my reasoning process. Please first select a human need that you find appropriate.",
+  //           trigger: "proceed-human-need"
+  //       },
+		// {
+		// 	id: "proceed-human-need",
+  //           options: [
+  //               { value: 'Food', label: 'Food', trigger: "confirm-new-human-need" },
+  //               { value: 'Social contact', label: 'Social contact', trigger: "confirm-new-human-need" },
+  //               { value: 'Health', label: 'Health', trigger: "confirm-new-human-need" },
+  //               { value: 'Savings', label: 'Savings', trigger: "confirm-new-human-need" },
+  //               { value: 'Approval', label: 'Approval', trigger: "confirm-new-human-need" },
+  //               { value: 'Order', label: 'Order', trigger: "confirm-new-human-need" },
+  //               { value: 'Safety', label: 'Safety', trigger: "confirm-new-human-need" },
+  //               { value: 'Romance', label: 'Romance', trigger: "confirm-new-human-need" },
+  //               { value: 'Belonging', label: 'Belonging', trigger: "confirm-new-human-need" },
+  //               { value: 'Power', label: 'Power', trigger: "confirm-new-human-need" },
+  //               { value: 'Safety', label: 'Safety', trigger: "confirm-new-human-need" },
+  //               { value: 'Family', label: 'Family', trigger: "confirm-new-human-need" },
+  //               { value: 'Competition', label: 'Competition', trigger: "confirm-new-human-need" },
+  //               { value: 'Honor', label: 'Honor', trigger: "confirm-new-human-need" },
+  //               { value: 'Status', label: 'Status', trigger: "confirm-new-human-need" },
+  //               { value: 'Curiosity', label: 'Curiosity', trigger: "confirm-new-human-need" },
+  //               { value: 'Serenity', label: 'Serenity', trigger: "confirm-new-human-need" },
+  //               { value: 'Idealism', label: 'Idealism', trigger: "confirm-new-human-need" },
+  //               { value: 'Independence', label: 'Independence', trigger: "confirm-new-human-need" },
+  //           ]
+		// },
+  //     	{
+		//     id: 'confirm-new-human-need',
+		//     message: "Are you sure the appropriate human need is {previousValue}?",
+		//     trigger: "last-of-human"
+	 //    },
+	 //    {
+		//     id: 'last-of-human',
+		//     message: "Great. Let's begin analyzing the rest of the AI's reasoning.",
+		//     trigger: "fin-human-need"
+	 //    },
+	 //    {
+	 //    	id: "fin-human-need",
+		//     options: 
+		// 	    [
+		// 	      { value: 1, label: "Begin", trigger: 'acceptable-step'},
+		// 	    ]
+	 //    },
+  //       /****************************************************/
 
-      	{
-		    id: 'confirm-new-human-need',
-		    message: "Are you sure the appropriate human need is {previousValue}?",
-		    trigger: "cont-human-need"
-	    },
-	    {
-            id: "cont-human-need",
-		    options: 
-		    [
-		      { value: 1, label: "Proceed"}
-		    ]
+        /****************************************************/
+        /*reasoning step analysis*/
+        /****************************************************/
+        {
+        	id: "acceptable-step",
+        	message: "Is this step in the AI's reasoning rational?",
+        	trigger: "rational-step"
+        },
+        {
+        	id: "rational-step",
+        	component: <ButtonReasoningLogic />                
+        },
+        //reasoning IS rational
+        {
+        	id: "accept-logic",
+        	message: "Great. You've selected that the previous reasoning performed by the AI is acceptable. Let's move to the next step.",
+        	trigger: "acceptable-step"
         }
     ];
 
@@ -186,7 +240,7 @@ export default function ChatBotPage() {
 			</Navbar>
 
 			<div style={{display: "flex", "justify-content": "space-between"}}>
-		        <ProSidebar collapsed="collapsed" style={{height: "100vh", display: "inline-block", float: "left"}}>
+		        <ProSidebar collapsed="collapsed" style={{height: "100vh", display: "inline-block", float: "left", height: "92vh"}}>
 		      	<SidebarHeader>
 			        <div	
 			          style={{
@@ -208,23 +262,30 @@ export default function ChatBotPage() {
 				  </Menu>
 				</ProSidebar>
 
-	        	<div class="chatbot" style={{height: "100%", display: "block", margin: "0 auto", width: "75%"}}>
-		            <Head>
-		                <title>Co-Reasoning ChatBot</title>
-		                <link rel="icon" href="/favicon.ico" />
-		            </Head>
-		            <main>
-		                <ChatBot
-		                    headerTitle="Collaboration with AI"
-		                    // speechSynthesis={{ enable: true, lang: 'en' }}
-		                    steps={chat_steps}
-		                    fontFamily="Helvetica"
-		                    width="100%"
-		                    height="100vh"
-		                />
-		            </main>
-		        </div>
-		        <div class="visualization" style={{float: "right", "borderStyle": "solid", width: "20%"}}>Visual</div>
+				<div class="chat-and-logic" style={{width: "100%", margin:"10px"}}>
+					<div class="visual-logic" style={{margin: "10px", "border": "1px solid gray"}}>
+						<p style={{margin: "10px", "backgroundColor": "lightgrey", "borderLeft": "6px solid red"}}>Current Logic</p>
+						<Logic />
+					</div>
+
+		        	<div class="chatbot" style={{display: "block", margin: "0 auto"}}>
+			            <Head>
+			                <title>Co-Reasoning ChatBot</title>
+			                <link rel="icon" href="/favicon.ico" />
+			            </Head>
+			            <main>
+			                <ChatBot
+			                    headerTitle="Collaboration with AI"
+			                    // speechSynthesis={{ enable: true, lang: 'en' }}
+			                    steps={chat_steps}
+			                    fontFamily="Helvetica"
+			                    width="100%"
+			                    height="73vh"
+			                />
+			            </main>
+			        </div>
+			    </div>
+		        <div class="visualization" style={{float: "right", "borderStyle": "solid", width: "20%", height: "92vh"}}>Visual</div>
 		    </div>
         </>
     );
